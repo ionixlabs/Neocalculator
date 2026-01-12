f0b251d8f86089dc6ba1f6af336da1409fcaca39
Ionix Labs <251453726+ionixlabs@users.noreply.github.com>
2026-01-12 19:50:01 +0530

    Add files via upload

---
+++ lib/widgets/keep_alive_wrapper.dart
@@ -0,0 +1,25 @@
+// keep_alive_wrapper.dart
+// Wrapper widget to keep child widgets alive in the widget tree for performance.
+
+import 'package:flutter/material.dart';
+
+/// KeepAliveWrapper ensures its child widget remains alive and is not disposed.
+class KeepAliveWrapper extends StatefulWidget {
+  const KeepAliveWrapper({super.key, required this.child});
+  final Widget child;
+
+  @override
+  State<KeepAliveWrapper> createState() => _KeepAliveWrapperState();
+}
+
+class _KeepAliveWrapperState extends State<KeepAliveWrapper>
+    with AutomaticKeepAliveClientMixin<KeepAliveWrapper> {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return widget.child;
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
